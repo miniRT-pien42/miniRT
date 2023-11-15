7c3ec0a3d8ccf954a14114a3f4ccb00c8cfa7e25
habvi <93956895+habvi@users.noreply.github.com>
2023-11-15 19:51:08 +0900

    add: vector utils

--- playground/ab/draw/includes/vector.h
+++ playground/ab/draw/includes/vector.h
@@ -20,7 +20,11 @@ typedef struct s_sphere {
 	double		r;
 }	t_sphere;
 
-double		vec_dot(t_vector a, t_vector b);
-t_vector	vec_subtract(t_vector a, t_vector b);
+t_vector	vec_add(const t_vector a, const t_vector b);
+t_vector	vec_subtract(const t_vector a, const t_vector b);
+double		vec_norm(const t_vector v);
+double		vec_dot(const t_vector a, const t_vector b);
+t_vector	vec_scalar(const t_vector v, double scalar);
+t_vector	vec_normalize(const t_vector v);
 
 #endif
--- playground/ab/draw/srcs/vector.c
+++ playground/ab/draw/srcs/vector.c
@@ -1,18 +1,40 @@
 #include "vector.h"
 
-double	vec_dot(t_vector a, t_vector b)
+t_vector	vec_add(const t_vector a, const t_vector b)
+{
+	const t_vector	ret_v = {a.x + b.x, a.y + b.y, a.z + b.z};
+
+	return (ret_v);
+}
+
+t_vector	vec_subtract(const t_vector a, const t_vector b)
+{
+	const t_vector	ret_v = {a.x - b.x, a.y - b.y, a.z - b.z};
+
+	return (ret_v);
+}
+
+double	vec_norm(const t_vector v)
+{
+	return (sqrt(v.x * v.x + v.y * v.y + v.z * v.z));
+}
+
+double	vec_dot(const t_vector a, const t_vector b)
 {
 	return (a.x * b.x + a.y * b.y + a.z * b.z);
 }
 
-// double	vec_norm(t_vector v)
-// {
-// 	return (sqrt(v.x * v.x + v.y * v.y + v.z * v.z));
-// }
-
-t_vector	vec_subtract(t_vector a, t_vector b)
+t_vector	vec_scalar(const t_vector v, double scalar)
 {
-	const t_vector	v = {a.x - b.x, a.y - b.y, a.z - b.z};
+	const t_vector	ret_v = {v.x * scalar, v.y * scalar, v.z * scalar};
 
-	return (v);
+	return (ret_v);
+}
+
+t_vector	vec_normalize(const t_vector v)
+{
+	const double	length = vec_norm(v);
+	const t_vector	ret_v = {v.x / length, v.y / length, v.x / length};
+
+	return (ret_v);
 }
