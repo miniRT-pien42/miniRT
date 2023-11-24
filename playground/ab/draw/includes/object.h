2f22981416a90f93475e7623a322761451109d76
habvi <93956895+habvi@users.noreply.github.com>
2023-11-25 04:53:25 +0900

    add(cylinder): is_intersect_cylinder()

--- playground/ab/draw/includes/object.h
+++ playground/ab/draw/includes/object.h
@@ -8,9 +8,19 @@ typedef struct s_sphere {
 	double		r;
 }	t_sphere;
 
+typedef struct s_cylinder {
+	t_vector	center; // 中心座標ベクトル
+	t_vector	axis_normal; // 法線ベクトル
+	double		r;
+	double		height;
+}	t_cylinder;
+
 typedef struct s_ray {
 	t_vector	position;
 	t_vector	direction;
 }	t_ray;
+
+/* ray */
+t_ray	calc_ray(const int y, const int x);
 
 #endif
