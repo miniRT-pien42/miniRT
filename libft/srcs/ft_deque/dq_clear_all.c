c29db4499f97a87f21c96797e8e785b6655fa843
habvi <93956895+habvi@users.noreply.github.com>
2023-11-30 06:28:04 +0900

    add: deque in libft for read_file

---
+++ libft/srcs/ft_deque/dq_clear_all.c
@@ -0,0 +1,22 @@
+#include "ft_deque.h"
+#include "libft.h"
+
+void	deque_clear_all(t_deque **deque, void (*del)(void *))
+{
+	t_deque_node	*node;
+	t_deque_node	*tmp;
+
+	if (deque_is_empty(*deque))
+	{
+		ft_free((void **)deque);
+		return ;
+	}
+	node = (*deque)->node;
+	while (node)
+	{
+		tmp = node;
+		node = node->next;
+		deque_clear_node(&tmp, del);
+	}
+	ft_free((void **)deque);
+}
