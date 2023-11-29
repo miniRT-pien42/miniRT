86380620387ad9f0ebfd5381a093a07ef76e4da6
habvi <93956895+habvi@users.noreply.github.com>
2023-11-30 04:51:43 +0900

    add: libft

---
+++ libft/srcs/ft_strndup.c
@@ -0,0 +1,13 @@
+#include "libft.h"
+
+char	*ft_strndup(const char *s, const size_t maxlen)
+{
+	const size_t	len_s = ft_strnlen(s, maxlen);
+	char			*dst;
+
+	dst = (char *)x_malloc(sizeof(char) * (len_s + 1));
+	if (dst == NULL)
+		return (NULL);
+	ft_strlcpy_void(dst, s, len_s + 1);
+	return (dst);
+}
