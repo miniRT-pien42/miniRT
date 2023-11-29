a6c6a9720052b9b20912855f698cbc4481d84f59
habvi <93956895+habvi@users.noreply.github.com>
2023-11-30 05:51:04 +0900

    replace malloc to x_malloc

--- srcs/camera/camera.c
+++ srcs/camera/camera.c
@@ -1,14 +1,12 @@
-#include <stdlib.h>
 #include "camera.h"
-#include "value_tmp.h"
+#include "libft.h"
+#include "value_tmp.h" // todo: rm
 
 t_camera	*init_camera(void)
 {
 	t_camera	*camera;
 
-	camera = (t_camera *)malloc(sizeof(t_camera));
-	if (camera == NULL)
-		return (NULL);
+	camera = (t_camera *)x_malloc(sizeof(t_camera));
 	camera->pos = (t_vector)CAMERA_POS;
 	camera->dir_n = (t_vector)CAMERA_DIR_N;
 	camera->fov = CAMERA_FOV;
--- srcs/sphere/sphere.c
+++ srcs/sphere/sphere.c
@@ -1,6 +1,6 @@
-#include <stdlib.h>
 #include "object.h"
-#include "value_tmp.h"
+#include "libft.h"
+#include "value_tmp.h" // todo: rm
 
 //todo: #3 spが複数ある場合はloopでまわす
 
@@ -8,9 +8,7 @@ static t_sphere	*init_sphere3_tmp(void)
 {
 	t_sphere	*sphere;
 
-	sphere = (t_sphere *)malloc(sizeof(t_sphere));
-	if (sphere == NULL)
-		return (NULL);
+	sphere = (t_sphere *)x_malloc(sizeof(t_sphere));
 	sphere->center = (t_vector)SPHERE3_CENTER;
 	sphere->diameter = SPHERE3_DIAMETER;
 	sphere->color = (t_rgb)SPHERE3_COLOR;
@@ -22,9 +20,7 @@ static t_sphere	*init_sphere2_tmp(void)
 {
 	t_sphere	*sphere;
 
-	sphere = (t_sphere *)malloc(sizeof(t_sphere));
-	if (sphere == NULL)
-		return (NULL);
+	sphere = (t_sphere *)x_malloc(sizeof(t_sphere));
 	sphere->center = (t_vector)SPHERE2_CENTER;
 	sphere->diameter = SPHERE2_DIAMETER;
 	sphere->color = (t_rgb)SPHERE2_COLOR;
@@ -32,14 +28,11 @@ static t_sphere	*init_sphere2_tmp(void)
 	return (sphere);
 }
 
-t_sphere	*init_sphere(char *line)
+t_sphere	*init_sphere(void)
 {
 	t_sphere	*sphere;
 
-	(void)line;//todo: #3
-	sphere = (t_sphere *)malloc(sizeof(t_sphere));
-	if (sphere == NULL)
-		return (NULL);
+	sphere = (t_sphere *)x_malloc(sizeof(t_sphere));
 	sphere->center = (t_vector)SPHERE1_CENTER;
 	sphere->diameter = SPHERE1_DIAMETER;
 	sphere->color = (t_rgb)SPHERE1_COLOR;
