d5d99186a5f4939ce9f354bc6b4d99a6a69cfa38
hachi868 <24986912+hachi868@users.noreply.github.com>
2023-11-30 23:07:04 +0900

    :dizzy:[update]plane malloc => x_malloc

--- srcs/plane/plane.c
+++ srcs/plane/plane.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include "libft.h"
 #include "object.h"
 #include "value_tmp.h"
 
@@ -7,9 +8,7 @@ t_plane	*init_plane(char *line)
 	t_plane	*plane;
 
 	(void)line;//todo: #3
-	plane = (t_plane *)malloc(sizeof(t_plane));
-	if (plane == NULL)
-		return (NULL);
+	plane = (t_plane *)x_malloc(sizeof(t_plane));
 	plane->type = PLANE1_TYPE;
 	plane->point = (t_vector)PLANE1_POINT;
 	plane->dir_n = (t_vector)PLANE1_DIR_N;
