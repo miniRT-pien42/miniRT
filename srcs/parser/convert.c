fc2df2a5f7e6ea41800ec271e13dd6e65936c8f7
habvi <93956895+habvi@users.noreply.github.com>
2023-12-27 03:25:51 +0900

    mv: move function to convert.c

--- srcs/parser/convert.c
+++ srcs/parser/convert.c
@@ -1,8 +1,28 @@
 #include "color.h"
 #include "libft.h"
 #include "parse.h"
+#include "result.h"
 #include "vector.h"
 #include <stdlib.h> // todo: rm (atof)
+
+// min: 0 ~ 255, max: 0 ~ 255
+uint8_t	convert_to_uint8_in_range(\
+				const char *s, const int min, const int max, t_result *result)
+{
+	int		num;
+	bool	ret;
+
+	ret = ft_atoi(s, &num);
+	if (!ret)
+	{
+		*result = FAILURE;
+		return (UNUSED);
+	}
+	if (min <= num && num <= max)
+		return ((uint8_t)num);
+	*result = FAILURE;
+	return (UNUSED);
+}
 
 t_vector	convert_line_to_vector(const char *line)
 {
