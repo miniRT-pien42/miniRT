3ee7ace3c0e16fdeb67b6cda2dbb699751d3e6fe
hachi868 <24986912+hachi868@users.noreply.github.com>
2023-12-30 01:34:57 +0900

    :dizzy:[update]get_closer_distance() Remove unnecessary else if

--- srcs/sphere/sphere_distance.c
+++ srcs/sphere/sphere_distance.c
@@ -12,10 +12,6 @@ static double	get_closer_distance(double discriminant, double *distances)
 	{
 		if (distances[0] > 0 && distances[1] > 0)
 			closer_distance = fmin(distances[0], distances[1]);
-		else if (fabs(distances[0]) < EPSILON && distances[1] > 0)
-			closer_distance = distances[1];
-		else if (fabs(distances[1]) < EPSILON && distances[0] > 0)
-			closer_distance = distances[0];
 		else
 			closer_distance = fmax(distances[0], distances[1]);
 	}
