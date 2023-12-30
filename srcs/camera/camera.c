5949432d368bc246b27ce4974584f9b3c31bef0a
habvi <93956895+habvi@users.noreply.github.com>
2023-12-30 18:15:38 +0900

    norm

--- srcs/camera/camera.c
+++ srcs/camera/camera.c
@@ -11,7 +11,8 @@ t_camera	*init_camera(const char **line, t_result *result)
 	*result = SUCCESS;
 	camera = (t_camera *)x_malloc(sizeof(t_camera));
 	camera->pos = convert_line_to_vector(line[0], result);
-	camera->dir_norm = init_normal_vector(line[1], DIR_N_MIN, DIR_N_MAX, result);
+	camera->dir_norm = \
+		init_normal_vector(line[1], DIR_N_MIN, DIR_N_MAX, result);
 	camera->fov = convert_to_uint8_in_range(line[2], FOV_MIN, FOV_MAX, result);
 	return (camera);
 }
