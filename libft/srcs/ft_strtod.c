ac02e50f75e8b1addfed9be307ec2db9cf46b4d5
habvi <93956895+habvi@users.noreply.github.com>
2023-12-30 19:38:27 +0900

    upadte(ft_strtod): allow max 10 digits of decimal. any non-digit char is invalid.

--- libft/srcs/ft_strtod.c
+++ libft/srcs/ft_strtod.c
@@ -1,5 +1,6 @@
 #include "libft.h"
 #include <stdbool.h>
+#include <stdint.h>
 
 static bool	is_sign(const int c)
 {
@@ -23,13 +24,25 @@ static void	convert_decimal_part(\
 								double *num, const char **s, bool *is_valid_num)
 {
 	double	inverse_power;
+	uint8_t	digits_of_decimal;
 
 	inverse_power = 0.1;
-	while (ft_isdigit(**s))
+	digits_of_decimal = 0;
+	while (ft_isdigit(**s) && digits_of_decimal < 10)
 	{
 		*is_valid_num = true;
 		*num += inverse_power * (**s - '0');
 		inverse_power /= 10;
+		digits_of_decimal++;
+		(*s)++;
+	}
+	while (**s)
+	{
+		if (!ft_isdigit(**s))
+		{
+			*is_valid_num = false;
+			return ;
+		}
 		(*s)++;
 	}
 }
@@ -78,8 +91,8 @@ static void	run_ft_strtod(char *s, double expect)
 	// valid
 	if (end_ptr != NULL)
 	{
-		printf("num    : [%.20f]\n", num);
-		printf("expect : [%.20f]\n", expect);
+		printf("num    : [%.10f]\n", num);
+		printf("expect : [%.10f]\n", expect);
 		printf("\n");
 		assert(num == expect);
 		return ;
@@ -91,6 +104,7 @@ static void	run_ft_strtod(char *s, double expect)
 
 int main(void)
 {
+	// error
 	run_ft_strtod("", 0);
 	run_ft_strtod("   ", 0);
 	run_ft_strtod(".", 0);
@@ -106,14 +120,28 @@ int main(void)
 	run_ft_strtod("123   ", 0);
 	run_ft_strtod("a123", 0);
 	run_ft_strtod("123a", 0);
+	run_ft_strtod("    1.23456789123456789", 0);
+	run_ft_strtod("1.23456789123456789   ", 0);
+	run_ft_strtod("    1.23456789123456789   ", 0);
+	run_ft_strtod("1.23456789123456789xxx", 0);
 
+	// valid
 	run_ft_strtod("123", 123);
 	run_ft_strtod("-123", -123);
 	run_ft_strtod("2147483647", 2147483647);
 	run_ft_strtod("2147483648", 2147483648);
 	run_ft_strtod("-2147483648", -2147483648);
 	run_ft_strtod("-2147483649", -2147483649);
-	run_ft_strtod("1.23456789123456789", 1.23456789123456789);
-	run_ft_strtod("-1.23456789123456789", -1.23456789123456789);
+	// just 10 digits
+	run_ft_strtod("123.0123456789", 123.0123456789);
+	run_ft_strtod("-123.0123456789", -123.0123456789);
+	// 11 digits
+	run_ft_strtod("123.01234567891", 123.0123456789);
+	run_ft_strtod("-123.01234567891", -123.0123456789);
+	run_ft_strtod("123.01234567897", 123.0123456789);
+	run_ft_strtod("-123.01234567897", -123.0123456789);
+	// more
+	run_ft_strtod("1.23456789123456789", 1.2345678912);
+	run_ft_strtod("-1.23456789123456789", -1.2345678912);
 }
 */
