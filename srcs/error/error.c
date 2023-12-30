cc54d605b6622b0a29adaa56d41ef81068e4f827
habvi <93956895+habvi@users.noreply.github.com>
2023-12-31 00:57:14 +0900

    fix(error): update error message  first

--- srcs/error/error.c
+++ srcs/error/error.c
@@ -1,10 +1,17 @@
+#include "error.h"
 #include "libft.h"
 #include <stdlib.h> // exit
 #include <unistd.h>
 
-void	error_exit(const char *message)
+void	putstr_with_endl(const char *message)
 {
 	ft_putstr_fd(message, STDERR_FILENO);
 	ft_putstr_fd("\n", STDERR_FILENO);
+}
+
+void	error_exit(const char *message)
+{
+	putstr_with_endl(ERR_MESSAGE);
+	putstr_with_endl(message);
 	exit(EXIT_FAILURE);
 }
