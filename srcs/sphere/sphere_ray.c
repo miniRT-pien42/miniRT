0591065af9673f5032027071d60d9fa2db93d8f4
hachi868 <24986912+hachi868@users.noreply.github.com>
2023-12-30 19:12:10 +0900

    :dizzy:[update]remove pow()

--- srcs/sphere/sphere_ray.c
+++ srcs/sphere/sphere_ray.c
@@ -15,7 +15,10 @@ static double	calc_b_for_sphere(const t_ray *ray, t_vector v)
 
 static double	calc_c_for_sphere(const t_sphere *sphere, t_vector v)
 {
-	return (pow(vec_length(v), 2) - pow(sphere->diameter / 2, 2));
+	const double len = vec_length(v);
+	const double radius = sphere->diameter / 2;
+
+	return (len * len - radius * radius);
 }
 
 double	calc_discriminant_for_sphere(\
@@ -25,7 +28,7 @@ double	calc_discriminant_for_sphere(\
 	const double	a = calc_a_for_sphere(ray);
 	const double	b = calc_b_for_sphere(ray, v);
 	const double	c = calc_c_for_sphere(sphere, v);
-	const double	d = pow(b, 2) - 4 * a * c;
+	const double	d = b * b - 4 * a * c;
 
 	calc_distance_by_discriminant(a, b, d, distances);
 	return (d);
