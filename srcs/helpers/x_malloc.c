e09cdd87416b255c3b26c837bed31b7380f397bf
habvi <93956895+habvi@users.noreply.github.com>
2023-12-31 00:50:28 +0900

    add(helpers): wrap functions that exit inside

--- srcs/helpers/x_malloc.c
+++ srcs/helpers/x_malloc.c
@@ -1,5 +1,6 @@
-#include <stdlib.h>
+#include "error.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 void	*x_malloc(const size_t size)
 {
@@ -8,6 +9,7 @@ void	*x_malloc(const size_t size)
 	ptr = (void *)malloc(size);
 	if (ptr == NULL)
 	{
+		putstr_with_endl(ERR_MESSAGE);
 		perror("malloc");
 		exit(EXIT_FAILURE);
 	}
