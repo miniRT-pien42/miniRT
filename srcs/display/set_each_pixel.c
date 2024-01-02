c1afeee8a081c583f6eb6ea14e90c6b17c465c3d
hachi868 <24986912+hachi868@users.noreply.github.com>
2024-01-02 22:13:43 +0900

    :dizzy:[update]rename func set_screen() => set_screen_to_camera_pos()

--- srcs/display/set_each_pixel.c
+++ srcs/display/set_each_pixel.c
@@ -5,7 +5,7 @@
 #include "scene.h"
 #include <stddef.h>
 
-static t_vector	set_screen(t_vector coord_on_screen, t_screen_info screen)
+static t_vector	set_screen_to_camera_pos(t_vector coord_on_screen, t_screen_info screen)
 {
 	coord_on_screen = rotate_vector_x(coord_on_screen, -screen.rotate_x);
 	coord_on_screen = rotate_vector_y(coord_on_screen, screen.rotate_y);
@@ -22,7 +22,7 @@ static t_ray	calc_ray(t_scene *scene, t_screen_info screen)
 	coord_on_screen.x = (2.0 * screen.x) / (WIDTH - 1) - 1.0;
 	coord_on_screen.y = 0.0;
 	coord_on_screen.z = -(2.0 * HEIGHT / WIDTH * screen.y) / (HEIGHT - 1) + 1.0;
-	coord_on_screen = set_screen(coord_on_screen, screen);
+	coord_on_screen = set_screen_to_camera_pos(coord_on_screen, screen);
 	ray.direction = vec_subtract(coord_on_screen, scene->camera->pos);
 	return (ray);
 }
